c32e145ca43dd071fef726514ebebabfe9533a08
Russell Troxel <russell@troxel.io>
2024-02-10 09:37:35 -0800

    Add dependencies
    Signed-off-by: Russell Troxel <russell@troxel.io>

--- clusters/bitty/bootstrap/bootstrap_flux.sh
+++ clusters/bitty/bootstrap/bootstrap_flux.sh
@@ -6,5 +6,6 @@ kubectl apply --server-side --filename https://raw.githubusercontent.com/prometh
 kubectl apply --server-side --filename https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${PROMETHEUS_OPERATOR_VERSION}/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml
 sops -d sops-age.bty.yaml | kubectl apply --server-side --filename -
 sops -d ../flux/vars/cluster-secrets.bty.yaml | kubectl apply --server-side --filename -
+sops -d ./github-deploy-key.bty.yaml| kubectl apply --server-side --filename -
 kubectl apply --server-side --kustomize ./
 kubectl apply --server-side --kustomize ../flux/config
